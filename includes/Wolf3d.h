7a8cbafdbb2c082dab1a476e18dae0528088489b
SegFault42 <rabougue@student.42.fr>
2016-05-11 16:24:32 +0200

    bug affichage wolf

--- includes/Wolf3d.h
+++ includes/Wolf3d.h
@@ -6,7 +6,7 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/09 12:52:14 by rabougue          #+#    #+#             */
-/*   Updated: 2016/05/10 20:31:13 by rabougue         ###   ########.fr       */
+/*   Updated: 2016/05/11 14:53:53 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,10 @@
 # include <math.h>
 # include "../libft/includes/libft.h"
 # include "./SDL2/SDL.h"
-# include "./SDL2/SDL_opengl.h"
+//# include "./SDL2/SDL_opengl.h"
 
-# define GL3_PROTOTYPES 1
-# define TRUE 1
-# define FALSE 0
+# define MAPWIDTH 24
+# define MAPHEIGHT 24
 
-
-typedef struct		s_sdl
-{
-	SDL_Window		*mainWindow;
-	SDL_GLContext	mainContext;
-}					t_sdl;
-/*
-** init.c
-*/
-int		init_sdl(t_sdl *sdl);
-int		SetOpenGLAttributes();
-/*
-** draw.c
-*/
-void	run_wolf(t_sdl *sdl);
-void	clean_up(t_sdl *sdl);
-void	check_sdl_error(int line);
 
 #endif
--- sources/init.c
+++ sources/init.c
@@ -6,37 +6,9 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/09 11:18:20 by rabougue          #+#    #+#             */
-/*   Updated: 2016/05/10 23:45:24 by rabougue         ###   ########.fr       */
+/*   Updated: 2016/05/11 09:22:42 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Wolf3d.h"
 
-int	init_sdl(t_sdl *sdl)
-{
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		ft_putendl("Init failed !");
-		return (0);
-	}
-	sdl->mainWindow = SDL_CreateWindow("Wolf3d", SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_OPENGL);
-	if (!sdl->mainWindow)
-	{
-		ft_putendl("Unable to create window");
-		return (0);
-	}
-	sdl->mainContext = SDL_GL_CreateContext(sdl->mainWindow);
-	SetOpenGLAttributes();
-	SDL_GL_SetSwapInterval(1);
-	return (1);
-}
-
-int	SetOpenGLAttributes()
-{
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	return (1);
-}
