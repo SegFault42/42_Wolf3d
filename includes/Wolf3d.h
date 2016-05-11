41e8458df60683ed42e925e55e0cba70c239e419
SegFault42 <rabougue@student.42.fr>
2016-05-11 19:05:08 +0200

    snake v0.1

--- includes/Wolf3d.h
+++ includes/Wolf3d.h
@@ -6,7 +6,7 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/09 12:52:14 by rabougue          #+#    #+#             */
-/*   Updated: 2016/05/11 18:09:22 by rabougue         ###   ########.fr       */
+/*   Updated: 2016/05/11 18:20:58 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@
 typedef struct	s_sdl_win
 {
 	SDL_Window		*win;
-	SDL_Renderer	*renderer;
+	SDL_Renderer	*render;
 	int				loop;
 }				t_sdl_win;
 
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/10 17:40:10 by rabougue          #+#    #+#             */
-/*   Updated: 2016/05/11 18:10:59 by rabougue         ###   ########.fr       */
+/*   Updated: 2016/05/11 19:01:36 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,13 @@ int	init_window(char *win_name, t_sdl_win *win)
 		ft_putendl(SDL_GetError());
 		return (1);
 	}
-	win->renderer = SDL_CreateRenderer(win->win, -1, SDL_RENDERER_SOFTWARE);
+	win->render = SDL_CreateRenderer(win->win, -1, SDL_RENDERER_SOFTWARE);
 	return (0);
 }
 
 void	close_window(t_sdl_win *win)
 {
+	SDL_DestroyRenderer(win->render);
 	SDL_DestroyWindow(win->win);
 	SDL_Quit();
 }
@@ -36,13 +37,18 @@ void	close_window(t_sdl_win *win)
 int main(int argc, char **argv)
 {
 	t_sdl_win win;
+	SDL_Rect	rectangle;
+	
+	rectangle.x = 500;
+	rectangle.y = 500;
+	rectangle.w = 50;
+	rectangle.h = 50;
 
 	init_window("Wolf3d", &win);
 	while (win.loop)
 	{
 		SDL_Event	event;
-		SDL_SetRenderDrawColor(win.renderer, 100, 250, 0, 255);
-		SDL_Rect	rectangle;
+		SDL_SetRenderDrawColor(win.render, 100, 250, 0, 255);
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_QUIT)
@@ -52,18 +58,20 @@ int main(int argc, char **argv)
 				if (event.key.keysym.sym == SDLK_ESCAPE)
 					win.loop = 0;
 				else if (event.key.keysym.sym == SDLK_UP)
-					rectangle.y--;
+					rectangle.y -= 50;
+				else if (event.key.keysym.sym == SDLK_DOWN)
+					rectangle.y += 50;
+				else if (event.key.keysym.sym == SDLK_RIGHT)
+					rectangle.x += 50;
+				else if (event.key.keysym.sym == SDLK_LEFT)
+					rectangle.x -= 50;
 			}
 		}
-		rectangle.x = 500;
-		rectangle.y = 500;
-		rectangle.w = 50;
-		rectangle.h = 50;
 
-		SDL_RenderFillRect(win.renderer, &rectangle);
-		SDL_RenderPresent(win.renderer);
-		SDL_SetRenderDrawColor(win.renderer, 0, 0, 0, 255);
-		SDL_RenderPresent(win.renderer);
+		SDL_RenderFillRect(win.render, &rectangle);
+		SDL_RenderPresent(win.render);
+		SDL_SetRenderDrawColor(win.render, 0, 0, 0, 255);
+		SDL_RenderPresent(win.render);
 	}
 	close_window(&win);
 	return (0);
